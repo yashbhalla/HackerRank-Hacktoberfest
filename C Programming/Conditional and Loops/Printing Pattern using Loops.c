fcd65b144dca0eead2bd7e8489bed4592e512868
Yash Bhalla <53651804+yashbhalla@users.noreply.github.com>
2020-06-26 11:07:35 +0530

    Printing Patterns using Loops

---
+++ C Programming/Conditional and Loops/Printing Pattern using Loops.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+void check(int i, int j, int first, int last, int n) {
+  if(n >=1 ){
+    if (i == first || i == last || j == first || j == last)
+        printf("%d ", n);
+    else
+        check(i, j, first + 1, last - 1, n - 1);
+  }
+}
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+      int rows = 2 * n - 1;
+
+    for (int i = 0; i < rows; i ++) {
+        for (int j = 0; j < rows; j ++) {
+            check(i, j, 0, rows - 1, n);
+        }
+        printf("\n");
+    }
+    return 0;
+}
