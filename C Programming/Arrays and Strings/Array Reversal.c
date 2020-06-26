27cdbc5f50b1f6ebb9a26d99d4a6e8ef2b556a40
Yash Bhalla <53651804+yashbhalla@users.noreply.github.com>
2020-06-26 11:14:27 +0530

    Array Reversal

---
+++ C Programming/Arrays and Strings/Array Reversal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i, j;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    
+    for(int i = 0, j = num-1; i <= j; i++, j --) {
+        int t = arr[i];
+        arr[i] = arr [j];
+        arr[j] = t;
+    }
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    /* Write the logic to reverse the array. */
+
+    
+    return 0;
+}
