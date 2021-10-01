bc70014e3c1526e9e62f915f4db1b41a342658fd
Yash Bhalla <yashbhalla2015@gmail.com>
2021-10-01 19:33:05 +0530

    Add files via upload

---
+++ Features.js
@@ -0,0 +1,12 @@
+Features of Node.js
+Following are some of the important features that make Node.js the first choice of software architects.
+
+Asynchronous and Event Driven − All APIs of Node.js library are asynchronous, that is, non-blocking. It essentially means a Node.js based server never waits for an API to return data. The server moves to the next API after calling it and a notification mechanism of Events of Node.js helps the server to get a response from the previous API call.
+
+Very Fast − Being built on Google Chrome's V8 JavaScript Engine, Node.js library is very fast in code execution.
+
+Single Threaded but Highly Scalable − Node.js uses a single threaded model with event looping. Event mechanism helps the server to respond in a non-blocking way and makes the server highly scalable as opposed to traditional servers which create limited threads to handle requests. Node.js uses a single threaded program and the same program can provide service to a much larger number of requests than traditional servers like Apache HTTP Server.
+
+No Buffering − Node.js applications never buffer any data. These applications simply output the data in chunks.
+
+License − Node.js is released under the MIT license
