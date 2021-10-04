501dee32af094b7af4a8f5ce492fe36d38f32042
Yash Bhalla <yashbhalla2015@gmail.com>
2021-10-04 10:53:07 +0530

    Add files via upload

--- Arrays.c
+++ Arrays.c
@@ -1,21 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
-int main() {
-    int a[1000];
-    int i, n, sum=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        sum=sum+a[i];
-    }
-    printf("%d",sum);
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-    return 0;
-}
+//Java Program to illustrate how to declare, instantiate, initialize  
+//and traverse the Java array.  
+class Testarray{  
+public static void main(String args[]){  
+int a[]=new int[5];//declaration and instantiation  
+a[0]=10;//initialization  
+a[1]=20;  
+a[2]=70;  
+a[3]=40;  
+a[4]=50;  
+//traversing array  
+for(int i=0;i<a.length;i++)//length is the property of array  
+System.out.println(a[i]);  
+}}  
